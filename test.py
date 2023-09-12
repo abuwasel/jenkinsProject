9b95d9d9ec4e5a8140f97b4e2cb8a46f85810c4c
Ibrahim Abu Wasel <i.abuwasel@gmail.com>
2023-09-12 14:09:55 +0300

    few changes

--- test.py
+++ test.py
@@ -2,11 +2,24 @@ import time
 import allure
 import pytest
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 class Demo(unittest.TestCase):
     @staticmethod
     def test_1():
         expected = 5
         actual = 5
-        assert actual == expected
+        assert actual == expected, 'not correct'
+
+    @staticmethod
+    def test_2():
+        driver = webdriver.Chrome()
+        driver.get('https://www.google.com')
+        time.sleep(2)
+        title = driver.title
+        assert title == 'Google'
+        #driver.find_element(By.CSS_SELECTOR, selector).click()
+
 
